c94e1b94508682a5122b57e9182c33ffb3a49563
Pashimati <Pashimatii@gmail.com>
2022-04-05 21:27:33 +0300

    validators my-profile

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-my-profile',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  profile: FormGroup
+
+  constructor() {
+    this.profile = new FormGroup({
+
+      "name": new FormControl("", Validators.pattern("[a-zA-Z ]{3}")),
+      "surname": new FormControl("", Validators.pattern("[a-zA-Z ]{3}")),
+      "sex": new FormControl(),
+      "age": new FormControl("", Validators.pattern("[0-9]{2}")),
+      "address": new FormControl("", Validators.pattern("[0-9][a-zA-Z ]{10}")),
+      "phone": new FormControl("", Validators.pattern("[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}")),
+    });
+
+  }
 
   ngOnInit(): void {
   }
 
+
+  submit() {
+    const data = this.profile.getRawValue()
+  }
 }

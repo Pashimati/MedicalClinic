11ce0e494a0ec76735a404c38331dd3d0c00fe21
Pashimati <Pashimatii@gmail.com>
2022-04-06 11:08:12 +0300

    validators my-profile fix bags footer

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 
+
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
@@ -13,12 +14,12 @@ export class MyProfileComponent implements OnInit {
   constructor() {
     this.profile = new FormGroup({
 
-      "name": new FormControl("", Validators.pattern("[a-zA-Z ]{3}")),
-      "surname": new FormControl("", Validators.pattern("[a-zA-Z ]{3}")),
-      "sex": new FormControl(),
-      "age": new FormControl("", Validators.pattern("[0-9]{2}")),
-      "address": new FormControl("", Validators.pattern("[0-9][a-zA-Z ]{10}")),
-      "phone": new FormControl("", Validators.pattern("[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}")),
+      name: new FormControl("", Validators.min(3)),
+      surname: new FormControl("", Validators.min(3)),
+      sex: new FormControl(),
+      age: new FormControl("", Validators.pattern("[0-9]{2}")),
+      address: new FormControl("", Validators.min(20)),
+      phone: new FormControl("", Validators.pattern("[- +()0-9]+")),
     });
 
   }
@@ -29,5 +30,6 @@ export class MyProfileComponent implements OnInit {
 
   submit() {
     const data = this.profile.getRawValue()
+    console.log(data)
   }
 }

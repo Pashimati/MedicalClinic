a907a03ed114d955533519751a0ec4bbe3ef4e7c
Pashimati <Pashimatii@gmail.com>
2022-04-12 21:06:41 +0300

    select doctors=department

--- src/app/components/form-appoitnment/form-appointment.component.ts
+++ src/app/components/form-appoitnment/form-appointment.component.ts
@@ -15,7 +15,6 @@ interface nameDepartment {
   templateUrl: './form-appointment.component.html',
   styleUrls: ['./form-appointment.component.scss'],
   providers: [
-    DataService,
     HttpService
   ]
 })
@@ -23,9 +22,14 @@ export class FormAppointmentComponent implements OnInit {
 
   appointment: FormGroup
 
+  doctors: Doctor [] = [];
+
+  allDoctors: Doctor [] = [];
+
+  nameDepartments: nameDepartment[] = [];
+
   constructor
   (
-    private dataService : DataService,
     private http : HttpService
   ) {
     this.appointment = new FormGroup({
@@ -47,11 +51,6 @@ export class FormAppointmentComponent implements OnInit {
     this.setDoctors()
   }
 
-  doctors: Doctor [] = [];
-
-  nameDepartments: nameDepartment[] = [];
-
-
   setNameDepartments () {
     this.http.getAll('https://api-medical-clinic.herokuapp.com/department/get-all')
       .subscribe({
@@ -69,19 +68,13 @@ export class FormAppointmentComponent implements OnInit {
       })
   }
 
-  // onChange(event: any) {
-  //   const department: string = event.value
-  //   this.doctors = this.getDoctors(department);
-  // }
-
-
-  setDoctors () {
+  setDoctors (): any {
     this.http.getAll('https://api-medical-clinic.herokuapp.com/doctor/get-all')
       .subscribe({
         next: ({response}: any) => {
           const doctors = response.doctors
 
-          this.doctors = doctors.map((doctor: any) => {
+          this.allDoctors = doctors.map((doctor: any) => {
             const data = doctor.data
             return {
               name: data.name,
@@ -94,42 +87,26 @@ export class FormAppointmentComponent implements OnInit {
   }
 
 
+  onChange(event: any) {
+    const department: string = event.value
+    this.doctors = this.getDoctors(department);
+  }
 
+  getDoctors(department: string) {
 
-  // getDoctors(department: string) {
-  //
-  //   let response: Doctor [] = []
-  //
-  //   const allDoctors: Doctor[] = this.dataService.getDoctors()
-  //   allDoctors.forEach((doctor) => {
-  //     if (doctor.department === department) {
-  //       response.push(doctor)
-  //     }
-  //   })
-  //  return response
-  // }
+    let response: Doctor [] = []
 
-
+    this.allDoctors.forEach((doctor) => {
+      if (doctor.department === department) {
+        response.push(doctor)
+      }
+    })
+   return response
+  }
 
   submit() {
     const data = this.appointment.getRawValue()
     console.log(data)
   }
 
-
 }
-
-// в первом селекте спрашивать категорию врача и отталкиваясь от категории врача показывать данных врачей
-
-
-//  создаю свойство называю Докторс
-//  создаю свойство называю Департментс это объект у которого ключ это валую а значение этоого объекта это описание для опшиона
-//  формирую список отделов ключ и значение Неврология и тд (самое важное ключи отделов совпадали с новым свойством внутри объекта врача)
-//  добавлю  свойство в объект с врачами чтобы оно совпадала с валую опшиона департмент
-//  вешаю событие на селект с отделами ончандже создаю функцию
-
-
-//  у измененого селекта находим текущий опшион и у него получаем валуе с именем отдела  валуе==ключ
-//  пишу функцию которая принимает параметр намеДепартмент  для ее работы подключить дата сервис вызову функцию и сохраню в переменную все врачи
-//  пробегаюсь по врачам в цикле нахожу всех врачей с нужным мне отделом отдел==тому отделу
-//  нужных врачей сохраняю во времнный массив и отфильтрованый масив записываю в переменную докторс и показываю ее
